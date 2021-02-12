b30a5b31e366e3e3d73ec56d78f52fa726207672
Adam Birse <adambirse@gmail.com>
2021-02-12 20:44:19 +0000

    blog list component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
 import About from "../components/about";
 import Blog from "../components/blog-list";
 
@@ -24,22 +21,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <About />
       <Blog allPostsData={allPostsData} />
-      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul> */}
-      {/* </section> */}
     </Layout>
   );
 }
--- test/components/blog-list.test.js
+++ test/components/blog-list.test.js
@@ -6,7 +6,7 @@ import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 Enzyme.configure({ adapter: new Adapter() });
 
 import * as React from "react";
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 var chai = require("chai");
 var expect = chai.expect;
 import Blog from "../../components/blog-list";
@@ -21,4 +21,20 @@ describe("Blog list component", () => {
     const wrap = shallow(<Blog allPostsData={allPostsData} />);
     expect(wrap.find("h2").text()).to.equal("Blog");
   });
+
+  it("should render with data", function () {
+    const wrap = shallow(<Blog allPostsData={allPostsData} />);
+    expectChildElements(wrap, 2);
+  });
+
+  it("should render with no data", function () {
+    const wrap = shallow(<Blog allPostsData={[]} />);
+    expect(wrap.find("h2").text()).to.equal("Blog");
+    expectChildElements(wrap, 0);
+  });
 });
+
+const expectChildElements = (wrap, no) => {
+  expect(wrap.find("Link").length == no).to.be.true;
+  expect(wrap.find("Date").length == no).to.be.true;
+};
