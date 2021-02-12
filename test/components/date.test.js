e6a3bdd8a3c0b592a86c11423733142cc5117a66
Adam Birse <adambirse@gmail.com>
2021-02-12 14:21:20 +0000

    enzyme added for react testing

---
+++ test/components/date.test.js
@@ -0,0 +1,19 @@
+import "jsdom-global/register"; //at the top of file, even before importing React
+/* setup.js */
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import * as React from "react";
+import { mount } from "enzyme";
+var chai = require("chai");
+var expect = chai.expect;
+import Date from "../../components/date";
+
+describe("Date component", () => {
+  it("should render without throwing an error", function () {
+    const wrap = mount(<Date dateString="2011-10-05T14:48:00.000Z" />);
+    expect(wrap.find("time").text()).to.equal("October 5, 2011");
+  });
+});
