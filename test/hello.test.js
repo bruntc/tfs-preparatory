30dbf540d1940e1f0f99093b14834105637ecdeb
Adam Birse <adambirse@gmail.com>
2021-02-12 12:39:38 +0000

    mocha unit test for hello api

---
+++ test/hello.test.js
@@ -0,0 +1,19 @@
+import { handler } from "../pages/api/hello";
+import { createMocks } from "node-mocks-http";
+var chai = require("chai");
+var expect = chai.expect;
+
+it("hello test", () => {
+  const { req, res } = createMocks({
+    method: "GET",
+  });
+  handler(req, res);
+
+  expect(res._getStatusCode()).to.equal(200);
+
+  expect(JSON.parse(res._getData())).to.deep.equal({
+    text: "Hello",
+  });
+});
+
+export {};
