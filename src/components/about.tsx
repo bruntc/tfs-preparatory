0092d0db3c79f0915a6cc18aba586bb2d2406d03
Adam Birse <adambirse@gmail.com>
2021-02-16 17:16:22 +0000

    use component example

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import utilStyles from '../styles/utils.module.css'
+import {Card} from './card'
 
 export default function About () {
   return (
     <section className={utilStyles.headingMd}>
       <p>About me</p>
+      <Card title="title" paragraph="aasdasdasdasd"/>
     </section>
   )
 }
